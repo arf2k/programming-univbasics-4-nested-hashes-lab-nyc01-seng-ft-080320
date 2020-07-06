a6b5a0e11ac3f1dc17202d6e5be274595c45aa31
Alex Foreman <foreman.alexander@gmail.com>
2020-07-06 17:09:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,13 @@ def adding_matz
       }
     }
 
+programmer_hash = {:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+return programmer_hash
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
